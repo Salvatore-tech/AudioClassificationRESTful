b40b97b377850d3dfe3721146e288a1cf91b65a0
salvatorestarace <salvatoretech99@gmail.com>
2023-03-03 22:24:27 +0100

    Code cleanup, audio samples for both target classes provided

--- src/__init__.py
+++ src/__init__.py
@@ -6,74 +6,27 @@ from src.classify import classify
 from src.shared import *
 from views import views
 
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # If the user does not select a file, the browser submits an
-#         # empty file without a filename.
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             return 'file uploaded successfully'
-#     return '''
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form method=post enctype=multipart/form-data>
-#       <input type=file name=file>
-#       <input type=submit value=Upload>
-#     </form>
-#     '''
-
-#
-# @app.route('/checkWhale/<track_name>', methods=['GET'])
-# def check_whale(track_name):
-#     full_path_to_track = os.path.join(UPLOAD_FOLDER, track_name)
-#     if not os.path.isfile(full_path_to_track):
-#         flash('File does not exists')
-#         return 'File does not exists'
-#     else:
-#         audio, _ = lr.load(full_path_to_track, sr=SAMPLE_RATE, res_type='kaiser_fast')
-#         mel_spectrogram = np.array(get_melspectrogram(audio))
-#         mel_spectrogram = tf.expand_dims(mel_spectrogram, axis=-1)
-#         prediction = np.round(model.predict(mel_spectrogram), 0)
-#         if prediction == 1:
-#             return 'That is a whale'
-#         return 'That is not a whale'
-#
-#
-# def allowed_file(filename):
-#     return '.' in filename and \
-#         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-#
-#
-
-
+MODEL_ARCHITECTURE = 'model_lighter.json'
+TRAINED_MODEL_WEIGHTS = 'weights_model_lighter.h5'
 
 if __name__ == '__main__':
     shared.init()
+
     # load json and create model
-    json_file = open('cnn_model/model_lighter.json', 'r')
+    json_file = open(os.path.join(DL_MODEL_FOLDER, MODEL_ARCHITECTURE), 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     shared.model = model_from_json(loaded_model_json)
+
     # load weights into new model
-    shared.model.load_weights('cnn_model/weights_model_lighter.h5')
+    shared.model.load_weights(os.path.join(DL_MODEL_FOLDER, TRAINED_MODEL_WEIGHTS), 'r')
     print("Loaded model from disk")
 
+    # register blueprints
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(classify, url_prefix="/classify")
 
-    # optimizer = keras.optimizers.Adam(lr=0.001)
     shared.model.compile(optimizer='adam', loss=keras.losses.binary_crossentropy,
-                  metrics=['accuracy'
-                           ])
+                         metrics=['accuracy'
+                                  ])
     app.run(debug=True)
--- src/shared.py
+++ src/shared.py
@@ -1,12 +1,16 @@
+import os
+
 from flask import Flask
 
 UPLOAD_FOLDER = './upload'
+RESOURCE_FOLDER = './resources'
+DL_MODEL_FOLDER = os.path.join(RESOURCE_FOLDER, 'dl_model_saved')
 ALLOWED_EXTENSIONS = {'aiff', 'wav'}
 SAMPLE_RATE = 2000
 
 app = Flask(__name__)
 app = Flask(__name__, template_folder='templates')
-app.config['MAX_CONTENT_LENGTH'] = 5 * 1000 * 1000
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1000 * 1000 # 5MB is the maximum upload file size
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "super secret key"
 
